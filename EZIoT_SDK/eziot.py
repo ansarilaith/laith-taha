798e2bb562b4398d650fda39cae70862740890f1
Clayton Darwin <claytondarwin@gmail.com>
2021-03-11 20:56:34 -0500

    eziot networks

--- EZIoT_SDK/eziot.py
+++ EZIoT_SDK/eziot.py
@@ -32,7 +32,10 @@ try:
     import time
     import urequests as requests
     import ujson as json
-    from urandom import randint   
+    from urandom import randint
+    import network
+    from ubinascii import hexlify
+    upython = True
 
 # default to regular python
 except:
@@ -40,6 +43,7 @@ except:
     import requests
     import json
     from random import randint
+    upython = False
 
 #----------------------------------------------
 # micropython-only imports
@@ -49,57 +53,17 @@ except:
 # use the imports below if you are only using
 # this module in a micropython device
 
+##import usys
+##import time
 ##import urequests as requests
 ##import ujson as json
-##from urandom import randint   
+##from urandom import randint
+##import network
+##from ubinascii import hexlify
+##upython = True
 
 #----------------------------------------------
-# network functions (not used directly)
-#----------------------------------------------
-
-def _check_error(code,jdata):
-
-    # error
-    if code != 200 or not jdata.get('success',False):
-        message = 'Request error: code: {}, detail: {}'.format(code,jdata.get('message','unknown reason'))
-        raise Exception(message)
-
-    # okay
-    return True
-
-def _make_request(route,data={},timeout=10):
-
-    # make path
-    route = '{}/v{}/{}'.format(api_base_path,float(api_version),route)
-
-    # set up auth
-    key,secret,n = None,None,randint(-16,32)
-    if api_key:
-        key = ''.join((chr(ord(c)+n) for c in api_key))
-    if api_secret:
-        secret = ''.join((chr(ord(c)+n) for c in api_secret))
-    auth = [key,secret,n]
-
-    # make packet
-    assert type(data) == dict
-    data['auth'] = auth
-    packet = json.dumps(data)
-
-    # request
-    resp = requests.post(route,data=packet,headers={'Content-Type':'application/json'},timeout=timeout)
-
-    # build return
-    code = resp.status_code
-    try:
-        jdata = resp.json()
-    except:
-        jdata = {'json_parse_error':resp.text}
-
-    # done
-    return code,jdata
-
-#----------------------------------------------
-# general functions
+# data functions
 #----------------------------------------------
 
 # get stats for data table
@@ -115,10 +79,6 @@ def stats():
 
     # return dict
     return jdata.get('stats',{})
-
-#----------------------------------------------
-# data functions
-#----------------------------------------------
 
 # insert a row of data, return rowid of insert
 def post_data(group=None,device=None,data1=None,data2=None,data3=None,data4=None):
@@ -154,13 +114,13 @@ def post_data(group=None,device=None,data1=None,data2=None,data3=None,data4=None
     return jdata.get('rowid',0)
 
 # get rows of data
-def get_data(count=1,after=None,group=None,device=None,sort=None):
-    '''Get rows of data. Limit return using "after", "group", "device".
+def get_data(count=1,after=None,group=None,device=None):
+    '''Get the "count" most-recent rows of data.
+    Limit return using "after", "group", "device".
     count = 1-1024 (how many rows to return)
     after = rowid (only rows after this rowid)
     group = "groupname" (only rows with this group name)
     device = "devicename" (only rows with this device name)
-    sort = "asc" or "desc" (sort on rowids, default descending)
     RETURN: list of data row lists [[row],[row],...]
     row = [rowid,epoch,gmt,ip,group,device,data1,date2,data3,data4]
     '''
@@ -174,7 +134,7 @@ def get_data(count=1,after=None,group=None,device=None,sort=None):
     data['count'] = count
 
     # check general data types and string lengths
-    for name,value in (('group',group),('device',device),('sort',sort)):
+    for name,value in (('group',group),('device',device)):
         if value != None:
             if type(value) == str:
                 assert len(value) <= 32
@@ -234,6 +194,114 @@ def delete_data(rowids=[],before=None,xall=False):
 
     # return rowid count
     return jdata.get('rows',0)
+
+#----------------------------------------------
+# network functions
+#----------------------------------------------
+
+# upython only wifi scan
+def wifi_scan():
+    if not upython:
+        return
+    wlan = network.WLAN(network.STA_IF)
+    state = wlan.active() # save current state
+    wlan.active(True) # set state active
+    for ssid,bssid,channel,RSSI,authmode,hidden in wlan.scan():
+        ssid = ssid.decode('ascii')
+        bssid = hexlify(bssid).decode('ascii')
+        if len(bssid) == 12:
+            bssid = ':'.join([bssid[x:x+2] for x in range(0,12,2)])
+        authmode = ('OPEN','WEP','WPA-PSK','WPA2-PSK','WPA/WPA2-PSK')[min(4,max(0,authmode))]
+        if hidden:
+            hidden = True
+        else:
+            False
+        print('Network AP:',[ssid,bssid,channel,RSSI,authmode,hidden])
+    wlan.active(state) # return to pervious state
+
+# upython-only wifi connect
+def wifi_connect(essid,password,timeout=15):
+    if not upython:
+        return False
+    print('Network Connect:',essid)
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        wlan.connect(essid,password)
+        time.sleep(0.1)
+        for x in range(timeout):
+            if wlan.isconnected():
+                break
+            time.sleep_ms(500)
+    return_value = wlan.isconnected()
+    print('Network Connect:',essid,return_value)
+    return return_value
+
+# upython-only wifi connect
+def wifi_disconnect(timeout=15):
+    if not upython:
+        return False
+    print('Network Disconnect')
+    wlan = network.WLAN(network.STA_IF)
+    return_value = True
+    if wlan.active():
+        if wlan.isconnected():
+            wlan.disconnect()
+            time.sleep(0.1)
+            for x in range(timeout):
+                if not wlan.isconnected():
+                    break
+                time.sleep(1)
+            return_value = not wlan.isconnected()
+    wlan.active(False)
+    print('Network Disonnect:',return_value)
+    return return_value
+
+# not a user function
+def _check_error(code,jdata):
+
+    # error
+    if code != 200 or not jdata.get('success',False):
+        message = 'Request error: code: {}, detail: {}'.format(code,jdata.get('message','unknown reason'))
+        raise Exception(message)
+
+    # okay
+    return True
+
+# not a user function
+def _make_request(route,data={},timeout=10):
+
+    # make path
+    route = '{}/v{}/{}'.format(api_base_path,float(api_version),route)
+
+    # set up auth
+    key,secret,n = None,None,randint(-16,32)
+    if api_key:
+        key = ''.join((chr(ord(c)+n) for c in api_key))
+    if api_secret:
+        secret = ''.join((chr(ord(c)+n) for c in api_secret))
+    auth = [key,secret,n]
+
+    # make packet
+    assert type(data) == dict
+    data['auth'] = auth
+    packet = json.dumps(data)
+
+    # request
+    if upython:
+        resp = requests.post(route,data=packet,headers={'Content-Type':'application/json'})
+    else:
+        resp = requests.post(route,data=packet,headers={'Content-Type':'application/json'},timeout=timeout)
+
+    # build return
+    code = resp.status_code
+    try:
+        jdata = resp.json()
+    except:
+        jdata = {'json_parse_error':resp.text}
+
+    # done
+    return code,jdata
 
 #----------------------------------------------
 # note: once you have your credentials, you can
