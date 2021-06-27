7f6633c35f20cc7766514bbe01851d4a2a5e534c
duder1966 <claytondarwin@gmail.com>
2021-06-27 16:45:27 -0400

    microserver.py

---
+++ MicroPython/MicroServer/main.py
@@ -0,0 +1,278 @@
+# example using eziot.link/dns and microserver
+# originally by claytondarwin@gmail.com
+
+#-------------------------------
+# variables
+#-------------------------------
+
+# network (add your credentials)
+essid    = 'youressid'
+password = 'yourpassword'
+
+# eziot (add your credentials)
+eziot_api_key = 'yourkey'
+eziot_api_secret = 'yoursecret'
+eziot_dnsid = 'yourdnsid'
+
+#-------------------------------
+# imports
+#-------------------------------
+
+import time
+import _thread
+from microserver import MicroServer
+
+#-------------------------------
+# not using a funboard
+#-------------------------------
+
+# if you are not not using a funboard ...
+
+# you need to do these imports
+# import eziot
+
+# you will need to create these functions
+# wifi.connect(essid,password)
+# rtc.ntp_set() is "from ntptime import settime"
+# esp32.reset()
+
+# you can comment out these (or replace with pass)
+# beeper.beep()
+# pixels.setp(0,'red')
+# pixels.off()
+
+# you will need to set up the /sd/htdocs dir
+# you can do it on the esp32 of mount an sdcard
+
+#-------------------------------
+# create server with application
+#-------------------------------
+
+class SERVER(MicroServer):
+
+    # server variables
+    htdocs = '/sd/htdocs'
+
+    # guest data
+    guestfile = '/sd/guests.csv'
+    guestlist = []
+    guestread = False
+    guestcount = 0
+    guestshow = 24
+
+    # redefine client thread open-close functions
+
+    # called when client connects
+    def client_on(self,rc=None):
+        # rc = remote connection count
+        #pass
+        beeper.beep()
+        pixels.setp(0,'red')
+
+    # called when client disconnects
+    def client_off(self,path=None):
+        # path = request PATH_INFO
+        #pass
+        pixels.off()
+
+    # shortcut for application
+    def make_response(self,content='Hello World',codestring='200 OK'):
+        content = str(content)
+        header = 'HTTP/1.1 {}\nContent-Type: text/plain\nContent-Length: {}\n\n'.format(codestring,len(content))
+        return bytes(header+content,'utf8')
+
+    # i'm re-defining the application function to do what i want
+    def application(self,client_data,client_address,client_count):
+
+        try:
+
+            # get app option
+            option = None
+            if client_data['PATH_INFO']:
+                option = client_data['PATH_INFO'].split('/')[-1].lower().strip()
+
+            # pagecount option
+            if option == 'pagecount':
+                yield self.make_response(client_count)
+
+            # guest
+            elif option == 'guest':
+
+                # register
+                name = client_data.get('name',[''])[0].replace('|',' ')[:32].strip()
+                if name:
+                    where = client_data.get('from',[''])[0].replace('|',' ')[:32].strip()
+                    mesg = client_data.get('mesg',[''])[0].replace('|',' ')[:64].strip()
+                    now = rtc.dtstamp.split()[0]
+                    self.guestcount += 1
+                    self.guestlist.append((now,name,where,mesg))
+                    if len(self.guestlist) > self.guestshow:
+                        self.questlist = self.guestlist[-self.guestshow:]
+                    with open(self.guestfile,'a') as f:
+                        f.write('{}|{}|{}|{}\n'.format(now,name,where,mesg))
+                        f.close()
+
+                ## # send index
+                ## for block in self.file_server(self.htdocs+'/index.html',cache=False):
+                ##     yield block
+
+                # redirect (this resets the browser path too)
+                # but it does make an additional request 
+                content = '307 Temporary Redirect'
+                location = '\nLocation: /index.html'
+                yield self.make_response(content,content+location)
+
+            # guestlist
+            elif option == 'guestlist':
+
+                # read file
+                if (not self.guestread) and self.isfile(self.guestfile):
+                    guestcount = 0
+                    guestshow = self.guestshow
+                    guestlist = []
+                    with open(self.guestfile,'r') as f:
+                        for line in f:
+                            line = line.split('|',3)
+                            if len(line) == 4:
+                                guestcount += 1
+                                guestlist.append(tuple(line))
+                                guestlist = guestlist[-guestshow:]
+                    self.guestlist = guestlist
+                    self.guestcount = guestcount
+                    self.guestread = True
+
+                # send data
+                table = '\n<table>'
+                for guest in self.guestlist[::-1]:
+                    table += '\n<tr><td class="nw">{}</td><td>{}</td><td>{}</td><td>{}</td></tr>'.format(*guest)
+                table += '\n<tr><td class="nw" colspan="4">Total Guests: {}</td></tr>'.format(self.guestcount)
+                table += '\n</table>\n'                
+                yield self.make_response(table)
+
+            # not implemented
+            else:
+                content = '501 Not Implemented'
+                yield self.make_response(content,content)
+
+        except Exception as e:
+            import sys
+            sys.print_exception(e)
+            content = '500 Internal Server Error'
+            yield self.make_response(content,content)        
+
+# make server instance
+server = SERVER()
+
+#-------------------------------
+# create a reboot thread
+#-------------------------------
+
+# this is optional - reboots daily
+
+##def reboot_thread_function():
+##
+##    print('REBOOT THREAD: start')
+##
+##    reset_hour = 6 # gmt
+##
+##    # loop
+##    loops = 0
+##    while not reboot_thread_kill:
+##
+##        # gurrent gmt
+##        y,M,d,h,m,s = time.gmtime()[:6]
+##
+##        # notify every 10 minutes
+##        loops += 1
+##        if loops >= 600:
+##            loops = 0
+##            H = reset_hour - h
+##            if H < 0:
+##                H += 24
+##            print('REBOOT IN: {:0>2}hrs {:0>2}mins {:0>2}secs'.format(H,60-m,60-s))
+##
+##        # skip boot condition
+##        if (y,M,d) != (2000,1,1):
+##            # hour and minute correct 
+##            if h,m == reset_hour,0:
+##                # second is correct
+##                if 0 <= s <= 10:
+##                    print('REBOOT THREAD: reset')
+##                    esp32.reset()
+##
+##        # pause
+##        time.sleep_ms(100)
+##        
+##    print('REBOOT THREAD: end')
+##        
+##reboot_thread_kill = False
+##reboot_thread = _thread.start_new_thread(reboot_thread_function,())
+
+#-------------------------------
+# loop forever
+#-------------------------------
+
+print('MAIN: start loop')
+
+while 1:
+
+    try:
+
+        # network
+        try:
+            assert wifi.connect(essid,password)
+        except:
+            print('Waiting on wifi connect.')
+            time.sleep_ms(1000)
+            continue
+
+        # set clock (may fail)
+        for x in range(100):
+            try:
+                assert rtc.ntp_set()
+                break
+            except:
+                pass
+
+        # eziot dns register
+        eziot.api_key = eziot_api_key
+        eziot.api_secret = eziot_api_secret
+        for x in range(10):
+            try:
+                dnsid = eziot.set_dns(dnsid=eziot_dnsid)
+                print('EZIoT DNS:',dnsid)
+                break
+            except:
+                time.sleep_ms(1000)
+
+        # start server
+        server.serve()
+
+        # cascade KeyboardInterrupt
+        if server.keyboard_interrupt:
+            raise KeyboardInterrupt
+
+        # pause
+        time.sleep_ms(1000)
+
+    # keyboard kill
+    except KeyboardInterrupt:
+        print('MAIN: KeyboardInterrupt: end all')
+        try:
+            reboot_thread_kill = True
+            reboot_thread.exit()
+        except:
+            pass
+        break
+
+    # any exception
+    except Exception as error:
+        print('MAIN: reset')
+        sys.print_exception(error)
+
+print('MAIN: end')
+
+
+#-------------------------------
+# end
+#-------------------------------
---
+++ MicroPython/MicroServer/microserver.py
@@ -0,0 +1,484 @@
+#-----------------------------------------------
+# notes
+#-----------------------------------------------
+
+# microserver.py is a small server for MicroPython
+# this was originally written by claytondarwin@gmail.com
+# clayton does not care what you do with it
+
+# REQUIRES:
+# wifi connection exists
+
+# LIMITS:
+# does not take advantage of async functions
+# header lines <= 256 characters
+# input <= 1024 characters
+# not set up for https (but you coule ssl-wrap the client socket)
+# only handles common static file types (everything else is application/octet-stream)
+
+# APPLICATION:
+# you define (re-define) the application
+# if path startswith /app/, application is called
+# user-defined application MUST accept 3 values
+# user-defined application MUST be an interator
+# user-defined application MUST return full content (header+data)
+# user-defined application MUST return bytes
+# application(client_data,client_address,client_count)
+# client_data is a dict of item:value pairs
+# client_address is a tuple where index 0 is the client ip
+# client_count is the total number of client connections since restart
+
+#-----------------------------------------------
+# setup
+#-----------------------------------------------
+
+# notify
+print('LOAD: microserver.py')
+
+# imports
+import os,sys,time,network,gc,socket
+from uselect import poll,POLLIN,POLLHUP,POLLERR
+gc.collect()
+
+#-----------------------------------------------
+# class
+#-----------------------------------------------
+
+class MicroServer:
+
+    #-------------------------------------------
+    # variables
+    #-------------------------------------------
+
+    # user-defined variables
+    host = '0.0.0.0'
+    port = 80
+    https = False
+    max_requests = 5 # client sockets
+    max_content = 1024 # from client socket
+    socket_timeout = 30 # client socket
+    htdocs = '/htdocs'
+
+    # static file cache directives
+    # seconds for client to cache files/images
+    cache_for = 60*60*24 # = 24 hours
+    # non-cached files (send no-cache header)
+    nocache = ['/index.html']
+
+    # process variables
+    keyboard_interrupt = False # loop ended on a KeyboardInterrupt
+    rc = 0 # client connection count
+    kill = False # kill server loop   
+    poll_timeout = 10 # seconds when polling client socket
+
+    #-------------------------------------------
+    # applications
+    #-------------------------------------------
+
+    # user-defined applications
+    # these are placeholders and should be replaced
+
+    # application MUST accept three input values
+    # application MUST be an iterator (use yield instead of return)
+    # application MUST yield the full response (header + data)
+    # the response MUST be bytes
+    def application(self,client_data,client_address,client_count):
+
+        data = 'Client {} at {}.\nNo user application is defined.\n\n'.format(client_count,client_address[0])
+        data = bytes(data,'latin1')
+
+        header = 'HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: {}\n\n'.format(len(data))
+
+        yield bytes(header,'latin1') + data
+
+    # called when client connects
+    def client_on(self,rc=None):
+        # rc = remote connection count
+        pass
+
+    # called when client disconnects
+    def client_off(self,path=None):
+        # path = request PATH_INFO
+        pass
+    
+    #-------------------------------------------
+    # server (serve forever)
+    #-------------------------------------------
+
+    def serve(self):
+
+        # check server address outside of loop and catch
+        self.server_address = socket.getaddrinfo(self.host,self.port)[0][-1]
+
+        # record keyboard interrupt
+        keyboard_interrupt = False
+
+        # restart forever (except KeyboardInterrupt or self.kill == True)
+        while 1:
+
+            # catch all
+            try:
+
+                # open server socket
+                server_socket = socket.socket()
+                server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+                server_socket.bind(self.server_address)
+                server_socket.listen(self.max_requests)
+                print('uServer socket listening +{} on addr {}.'.format(self.max_requests,self.server_address))
+
+                # accept request loop
+                while 1:
+
+                    # kill flag
+                    if self.kill:
+                        print('uServer KILL flag set.')
+                        break
+
+                    # accept a request
+                    # get client socket (i.e. request socket)
+                    client_socket,client_address = server_socket.accept()
+                    t1 = time.ticks_ms()
+                    now = '{}-{}-{}_{}:{}:{}'.format(*time.localtime()[:6])
+                    self.rc += 1
+                    self.client_on(self.rc)
+                    client_socket.settimeout(self.socket_timeout) # 0
+                    print('uServer Client {}: {} - {} - M{}%.'.format(self.rc,client_address[0],now,self.memp()))
+                    bytecount = 0
+
+                    # parse all input data
+                    client_data = self.parse_socket_data(client_socket,client_address)
+                    print('uServer Client {} REQUEST: {} {} {} {} {}'.format(
+                        self.rc,
+                        client_data['SERVER_PROTOCOL'],
+                        client_data['REQUEST_METHOD'],
+                        client_data['PATH_INFO'],
+                        client_data['Content-Length'],
+                        client_data['Content-Type'],
+                        ))
+
+                    # path
+                    path = client_data['PATH_INFO'] or '/index.html'
+                    if path == '/':
+                        path = '/index.html'
+
+                    # static file
+                    if path and self.isfile(self.htdocs+path):
+                        cache = not (path in self.nocache)
+                        print('    File Request: {}'.format(path))
+                        for block in self.file_server(self.htdocs+path,cache=cache):
+                            bytecount += client_socket.write(block)
+
+                    # application iterator
+                    elif path and path[:5] in ('/app','/app/'):
+                        print('    Application Request')
+                        for block in self.application(client_data,client_address,self.rc):
+                            bytecount += client_socket.write(block)
+
+                    # unknown
+                    else:
+                        print('    Unknown Request')
+                        bytecount += client_socket.write(bytes('HTTP/1.1 404 Not Found\nContent-Type: text/plain\nContent-Length: 14\n\n404 Not Found\n','utf8'))
+
+                    # close client socket
+                    client_socket.close()
+                    self.client_off(path)
+                    print('uServer Client {}: {} - M{}% - {} bytes in {} secs.'.format(self.rc,client_address[0],self.memp(),bytecount,round(time.ticks_diff(time.ticks_ms(),t1)/1000.0,2)))
+                    gc.collect()
+                    
+            # keyboard kill
+            except KeyboardInterrupt:
+                keyboard_interrupt = True
+                print('uServer KeyboardInterrupt: End server loop.')
+                break
+
+            # any other exception
+            except Exception as error:
+                print('uServer Exception: Go to socket reset.')
+                sys.print_exception(error)
+
+            # must do
+            finally:
+
+                # close main socket
+                try:
+                    print('uServer Closing main socket...',end=' ')
+                    server_socket.close()
+                    print('closed.')
+                except:
+                    print('uServer FAILED!')
+
+                # clean up
+                self.client_off()
+                gc.collect()
+ 
+    #-------------------------------------------
+    # server sub-functions
+    #-------------------------------------------
+
+    def memp(self,collect=False):
+        if collect:
+            gc.collect()
+        free  = gc.mem_free()
+        alloc = gc.mem_alloc()
+        size = free+alloc
+        return int(round(100*alloc/size,0))
+
+    def isfile(self,f):
+        try:
+            stats = os.stat(f)
+            if oct(stats[0])[-5] != '4':
+                return True
+            else:
+                return False
+        except:
+            return False
+
+    def file_server(self,path,cache=False):
+
+        # file must exist
+        # ext-based file type
+
+        ext = None
+        if '.' in path:
+            ext = path.rsplit('.',1)[1].lower()
+
+        # make header
+        header  = 'HTTP/1.1 200 OK\n'
+        if cache is True:
+            header += 'Cache-Control: max-age={}\n'.format(self.cache_for)      
+        elif cache in (None,0):
+            header += 'Cache-Control: no-cache\n'
+        elif type(cache) == int and cache > 0:
+            header += 'Cache-Control: max-age={}\n'.format(cache)
+        header += 'Content-Type: {}\n'.format(self.file_types.get(ext,'application/octet-stream'))
+        header += 'Content-Length: {}\n'.format(os.stat(path)[6])
+
+        # send header
+        yield bytes(header+'\n','utf8')
+
+        # send file
+        with open(path,'rb') as f:
+            while 1:
+                data = f.read(1024)
+                if data:
+                    yield data
+                else:
+                    break
+            f.close()
+
+    file_types = {
+    'html':'text/html',
+    'css':'text/css',      
+    'txt':'text/plain',
+    'csv':'text/plain',      
+    'json':'application/json',
+    'pdf':'application/pdf',
+    'ico':'image/x-icon',
+    'jpg':'image/jpeg',
+    'png':'image/png',
+    'svg':'image/svg+xml',
+    'gif':'image/gif',
+    }
+
+    def parse_socket_data(self,client_socket,client_address,show=False):
+
+        # register client socket
+        poller = poll()
+        poller.register(client_socket,POLLIN)
+
+        # start query data
+        query_data =  {
+        'REMOTE_ADDR':client_address[0],
+        'SERVER_PROTOCOL':None,
+        'REQUEST_METHOD':None,
+        'PATH_INFO':None,
+        'Content-Length':0,
+        'Content-Type':None
+        }
+
+        # read header lines up to '\n\n'
+        # add each line into query_data
+        while 1:
+            polldata = poller.poll(int(self.poll_timeout*1000))
+            line = None
+            if not polldata:
+                print('    HEADER <NO_DATA>')
+                break
+            elif polldata[0][1] in (POLLHUP,POLLERR):
+                print('    HEADER <POLLHUP_POLLERR>')
+                break
+            else:
+                line = client_socket.readline()[:256]
+                if not line or line == b'\r\n':
+                    break
+                if show:
+                    print('    HEADER_LINE:',[line])
+                line = line.decode('utf8','replace').strip()
+                # GET
+                if line.startswith('GET '):
+                    query_data['REQUEST_METHOD'] = 'GET'
+                    line = line.split()
+                    if len(line) >= 3:
+                        query_data['SERVER_PROTOCOL'] = line[2]
+                        path = line[1]
+                        if '?' in path:
+                            path,qs = path.split('?',1)
+                            self.parse_content(qs,query_data)
+                        path = path.strip()
+                        query_data['PATH_INFO'] = path
+                # POST
+                elif line.startswith('POST '):
+                    query_data['REQUEST_METHOD'] = 'POST'
+                    line = line.split()
+                    if len(line) >= 3:
+                        query_data['SERVER_PROTOCOL'] = line[2]
+                        query_data['PATH_INFO']       = line[1]
+                # Content-Length:
+                elif line.startswith('Content-Length'):
+                    line = line.split()
+                    if len(line) >= 2 and line[1].isdigit():
+                        query_data['Content-Length'] = int(line[1])
+                # Content-Type:
+                elif line.startswith('Content-Type'):
+                    line = line.split()
+                    if len(line) >= 2:
+                        query_data['Content-Type'] = ' '.join(line[1:])
+
+        # handle a POST (extra data)
+        if query_data['REQUEST_METHOD'] == 'POST':
+            # multipart
+            if 'multipart' in query_data['Content-Type']:
+                pass
+            # no content length
+            elif not query_data['Content-Length']:
+                pass
+            # read data
+            else:
+                clen = query_data['Content-Length']
+                if self.max_content:
+                    clen = min(clen,self.max_content)
+                dlen = 0
+                data = b''
+                while dlen < clen:
+                    polldata = poller.poll(int(self.poll_timeout*1000))
+                    if not polldata:
+                        print('    POST <NO_DATA>')
+                        break
+                    elif polldata[0][1] in (POLLHUP,POLLERR):
+                        print('    POST <POLLHUP_POLLERR>')
+                        break
+                    else:
+                        data += client_socket.recv(clen-dlen)
+                        dlen = len(data)
+                self.parse_content(data,query_data)
+
+        # un-register client socket
+        poller.unregister(client_socket)
+        del poller
+
+        # print data
+        if show:
+            keys = list(query_data.keys())
+            keys.sort()
+            for key in keys:
+                print('    INPUT:',key,str(query_data[key])[:96])
+            del keys           
+
+        # done
+        return query_data
+
+    def parse_content(self,content,qd,final=False):
+
+        # decode
+        if type(content) == bytes:
+            content = content.decode('utf8','replace')
+
+        # split into parts on &
+        if '&' in content:
+            parts = content.split('&')
+        else:
+            parts = [content]
+
+        # clean
+        del content
+        gc.collect()
+
+        # parse parts to qd
+        for part in parts:
+            if part:
+                key,value = (part.split('=',1)+[''])[:2]
+                if key.strip():
+                    value,rest = self.url_unquote(value)
+                    if key in qd:
+                        qd[key].append(value)
+                    else:
+                        qd[key] = [value]
+
+    def url_unquote(self,text):
+
+        # replace url escape sequences in text
+        # return unquoted_text,remaining_text
+        # the remaining_text can be added to the next block
+
+        # the remaining_text is 0 to 2 chars in length
+        # this text is missing necessary characters
+        # i.e. the input text ended with % or %X, not %XX
+
+        rest = ''
+        if '%' in text[-2:]:
+            rest = text[-2:]
+            text = text[:-2]
+
+        if '+' in text:
+            text = text.replace('+',' ')
+
+        while '%' in text:
+            i = text.index('%')
+            c1 = text[i:i+3]
+            c2 = self.url_unquote_values.get(c1,'?')
+            text = text.replace(c1,c2)
+
+        return text,rest
+
+    # url unquote values
+    url_unquote_values = {
+    '%0D':'\r',
+    '%0A':'\n',
+    '%20':' ',
+    '%21':'!',
+    '%22':'"',
+    '%23':'#',
+    '%24':'$',
+    '%25':'%',
+    '%26':'&',
+    '%27':"'",
+    '%28':'(',
+    '%29':')',
+    '%2A':'*',
+    '%2B':'+',
+    '%2C':',',
+    '%2D':'-',
+    '%2E':'.',
+    '%2F':'/',
+    '%3A':':',
+    '%3B':';',
+    '%3C':'<',
+    '%3D':'=',
+    '%3E':'>',
+    '%3F':'?',
+    '%40':'@',
+    '%5B':'[',
+    '%5C':'\\',
+    '%5D':']',
+    '%5E':'^',
+    '%5F':'_',
+    '%60':'`',
+    '%7B':'{',
+    '%7C':'|',
+    '%7D':'}',
+    '%7E':'~',
+    }
+
+#-----------------------------------------------
+# end
+#-----------------------------------------------
