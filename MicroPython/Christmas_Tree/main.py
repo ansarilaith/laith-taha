4486e9ae26c73be4fc572aea349db5a722538d2d
duder1966 <claytondarwin@gmail.com>
2022-12-19 16:01:43 -0500

    christmas tree

--- MicroPython/Christmas_Tree/main.py
+++ MicroPython/Christmas_Tree/main.py
@@ -33,48 +33,65 @@ def main():
 
     # set up pixels (global)
     pixels = PIXELS(pixel_pin,pixel_count)
-    pixels.run(color='red',brightness=brightness,ontime=10,offtime=0)
     pixels.off()
+    print('PIXELS READY')
 
-    # for later use
+    # fixed colors for later use
     r = pixels.make_color('red',int(brightness/2))
     g = pixels.make_color('green',int(brightness/2))
     z = (0,0,0)
 
+    # run 2 directions
+    print('PIXELS R-G RUN')
+    for x in range(pixel_count):
+        pixels.np[x] = r
+        pixels.np[pixel_count-1-x] = g
+        pixels.np.write()
+        time.sleep_ms(10)
+        pixels.np[x] = z
+        pixels.np[pixel_count-1-x] = z
+        pixels.np.write()            
+    pixels.off()
+
     # loop
     while 1:
 
-        # flash about 10 seconds
-        pixels.rblink('red green sun'.split(),pixels=4,times=222,brightness=brightness,ontime=44,offtime=0)
+        # flash about 4 seconds
+        print('PIXELS FLASH')
+        pixels.rblink('red green sun'.split(),pixels=4,times=91,brightness=brightness,ontime=44,offtime=0)
         pixels.off()
 
-        # follow
+        # follow up for about 26 seconds
+        print('PIXELS RUN UP')
         for x in range(0,pixel_count,10):
             pixels.np[x] = r
         for x in range(5,pixel_count,10):
             pixels.np[x] = g
         pixels.np.write()
-        for x in range(int(pixel_count/4)):
+        for x in range(260):
             end = pixels.np.buf[-3:]
             pixels.np.buf = end + pixels.np.buf[:-3]
             pixels.np.write()
             time.sleep_ms(100)
-        for x in range(int(pixel_count/4)):
+        pixels.off()
+
+        # flash about 4 seconds
+        print('PIXELS FLASH')
+        pixels.rblink('red green sun'.split(),pixels=4,times=91,brightness=brightness,ontime=44,offtime=0)
+        pixels.off()
+
+        # follow down for about 26 seconds
+        print('PIXELS RUN DOWN')
+        for x in range(0,pixel_count,10):
+            pixels.np[x] = r
+        for x in range(5,pixel_count,10):
+            pixels.np[x] = g
+        pixels.np.write()
+        for x in range(260):
             end = pixels.np.buf[:3]
             pixels.np.buf = pixels.np.buf[3:]+end
             pixels.np.write()
             time.sleep_ms(100)
-        pixels.off()
-
-        # run 2 directions
-        for x in range(pixel_count):
-            pixels.np[x] = r
-            pixels.np[pixel_count-1-x] = g
-            pixels.np.write()
-            time.sleep_ms(10)
-            pixels.np[x] = z
-            pixels.np[pixel_count-1-x] = z
-            pixels.np.write()            
         pixels.off()
 
 #-----------------------
